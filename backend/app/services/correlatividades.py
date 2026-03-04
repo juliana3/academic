8ab20e455e35df83c55661f77a1216cbc01a7990
juliana3 <araujojuli1234@gmail.com>
2026-03-04 11:30:18 -0300

    listas las correlatividades

--- backend/app/services/correlatividades.py
+++ backend/app/services/correlatividades.py
@@ -1,1 +1,40 @@
 # verifica si una materia esta habilitada
+
+from ..enums import CondicionRequisito, EstadoMateria, ParaRequisito
+
+def esta_habilitada_para_cursar(materia, requisitos, materias_del_plan) -> bool:
+    for r in requisitos:
+        if r.para == ParaRequisito.cursar:
+            materia_requerida = [m for m in materias_del_plan if m.id == r.id_materia_req]
+            if not materia_requerida:
+                return False
+            
+            if r.condicion == CondicionRequisito.aprobada:
+                if materia_requerida[0].estado != EstadoMateria.aprobada:
+                    return False
+                
+            if r.condicion == CondicionRequisito.regular:
+                if materia_requerida[0].estado not in [EstadoMateria.regular, EstadoMateria.promocionada, EstadoMateria.aprobada]:
+                    return False
+    
+    return True
+
+
+def esta_habilitada_para_rendir(materia, requisitos, materias_del_plan) -> bool:
+    for r in requisitos:
+        if r.para == ParaRequisito.rendir_final:
+            materia_requerida = [m for m in materias_del_plan if m.id == r.id_materia_req]
+
+            if not materia_requerida:
+                return False
+            
+            if r.condicion == CondicionRequisito.aprobada:
+                if materia_requerida[0].estado != EstadoMateria.aprobada:
+                    return False
+                
+            if r.condicion == CondicionRequisito.regular:
+                if materia_requerida[0].estado not in [EstadoMateria.regular, EstadoMateria.promocionada, EstadoMateria.aprobada]:
+                    return False
+    
+    return True
+
