09d7bc1a0140cb3f2faebb4f575dae43bbccc30d
juliana3 <araujojuli1234@gmail.com>
2026-03-04 12:19:23 -0300

    listas las superposiciones de horarios

--- backend/app/services/horarios.py
+++ backend/app/services/horarios.py
@@ -1,1 +1,11 @@
 #verifica superposicion de horarios
+
+def detectar_superposiciones(horario_nuevo, todos_los_horarios) -> list:
+    superposiciones = [] #guarda los horarios existentes con los que se superpone el nuevo horario
+
+    for h in todos_los_horarios:
+        if h.dia_semana == horario_nuevo.dia_semana:
+            if h.hora_inicio < horario_nuevo.hora_fin and horario_nuevo.hora_inicio < h.hora_fin:
+                superposiciones.append(h)
+
+    return superposiciones
